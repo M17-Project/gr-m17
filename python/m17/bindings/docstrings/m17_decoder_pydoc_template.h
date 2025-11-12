5801da213cfefd9c830f88595f490eb5f0206efc
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2025-11-12 14:03:30 +0100

    bindings update

--- python/m17/bindings/docstrings/m17_decoder_pydoc_template.h
+++ python/m17/bindings/docstrings/m17_decoder_pydoc_template.h
@@ -14,47 +14,34 @@
   and will be overwritten during the build process
  */
 
+static const char *__doc_gr_m17_m17_decoder = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_m17_decoder_0 = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_m17_decoder_1 = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_make = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_m17_decoder_0 = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_set_debug_data = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_set_debug_ctrl = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_m17_decoder_1 = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_set_callsign = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_set_sw_threshold = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_make = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_set_vt_threshold = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_set_signed = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_set_debug_data = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_set_key = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_set_seed = R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_set_debug_ctrl = R"doc()doc";
+static const char *__doc_gr_m17_m17_decoder_parse_raw_key_string = R"doc()doc";
 
+static const char *__doc_gr_m17_m17_decoder_scrambler_sequence_generator =
+    R"doc()doc";
 
- static const char *__doc_gr_m17_m17_decoder_set_callsign = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_set_threshold = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_set_signed = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_set_key = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_set_seed = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_parse_raw_key_string = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_scrambler_sequence_generator = R"doc()doc";
-
-
- static const char *__doc_gr_m17_m17_decoder_scrambler_seed_calculation = R"doc()doc";
-
-  
+static const char *__doc_gr_m17_m17_decoder_scrambler_seed_calculation =
+    R"doc()doc";
